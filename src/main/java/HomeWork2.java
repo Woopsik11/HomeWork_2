8a15a194a03aff24ba14ffa3bf2364b1448dabe4
Woopsik11 <vantage59@iclouc.com>
2021-10-30 14:40:33 +0500

    11

---
+++ src/main/java/HomeWork2.java
@@ -0,0 +1,29 @@
+public class HomeWork2 {
+    public static void main(String[] args) {
+        System.out.println(twoNumbers(5, 9));
+        System.out.println(isPositive(11));
+        System.out.println(isNegative(23));
+        String s = "Hi";
+        int counter = 5;
+        printNumbers("Hi", 5);
+    }
+    private static boolean twoNumbers ( int first, int second) {
+        int sum = first + second;
+        return sum <= 20 && sum >= 10;
+    }
+
+
+    private static boolean isPositive(int variable) {
+        return variable >= 0;
+    }
+
+    private static boolean isNegative(int number) {
+        return number < 0;
+    }
+
+    private static void printNumbers(String s, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(s);
+        }
+    }
+}
